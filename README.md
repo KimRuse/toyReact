fae13927b2f798abac63f0a34f20cfa6bf74c2c0
sunzhiwen <1327192007@qq.com>
2020-08-24 09:26:48 +0800

    feat: 渲染JSX到DOM

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 
 # 项目初始化
 配置环境，支持热编译
+
+# render JSX to DOM
+渲染JSX 到 DOM
+- createElement方法用于生成element对象
+- createTextElement方法用于处理字符串
+- render方法用于递归element创建dom并给dom赋值props,通过document.appendChild链接各节点
--- src/index.js
+++ src/index.js
@@ -1,1 +1,59 @@
-console.log('hello')
+function createElement(type, props, ...children) {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map(item =>
+        typeof item === 'object'
+          ? item
+          : createTextElement(item)    
+      )
+    }
+  }
+}
+
+function createTextElement(text) {
+  return {
+    type: 'TEXT_ELEMENT',
+    props: {
+      nodeValue: text,
+      children: []
+    }
+  }
+}
+
+function render(element, container) {
+  const dom =
+    element.type === 'TEXT_ELEMENT'
+    ? document.createTextElement('')
+    : document.createElement(element.type)
+
+  element.props.children.forEach(child => {
+    render(child, dom)
+  })
+
+  const isProperty = key => key !== 'children'
+  Object.keys(element.props)
+    .filter(isProperty)
+    .forEach(
+      name => dom[name] = element.props[name]
+    )
+
+  container.appendChild(dom)
+}
+
+const React = {
+  render,
+  createElement
+}
+
+const element = React.createElement(
+  'h1',
+  { id: 'container' },
+  React.createElement('div', null, 'hello'),
+  'word'
+)
+
+const container = document.querySelector('#root')
+
+React.render(element, container)
