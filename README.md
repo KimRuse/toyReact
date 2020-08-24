df17898ca62e67542196e1a71729435be22e0853
ZhiWS <1327192007@qq.com>
2020-08-24 20:52:32 +0800

    feat: 构建Fiber

--- README.md
+++ README.md
@@ -9,3 +9,12 @@
 - createElement方法用于生成element对象
 - createTextElement方法用于处理字符串
 - render方法用于递归element创建dom并给dom赋值props,通过document.appendChild链接各节点
+
+# Fiber
+将element对象所表示的树结构
+-> 转换成由指针链接的链表树结构
+这样可以将不可终止的递归调用element对象的遍历行为
+-> 转换为可以拆解的fiber工作单元所连接的fiber树
+的调用。
+给出第一个rootFiber作为第一个nextUnitOfWork
+通过performUnitOfWork方法调用nextUnitOfWork,并返回下一个需要执行的nextUnitOfWork,我们可以设置条件来决定是否继续执行performUnitOfWork
--- src/index.js
+++ src/index.js
@@ -22,24 +22,87 @@ function createTextElement(text) {
   }
 }
 
-function render(element, container) {
+function createDom(fiber) {
   const dom =
-    element.type === 'TEXT_ELEMENT'
-    ? document.createTextElement('')
-    : document.createElement(element.type)
+    fiber.type === 'TEXT_ELEMENT'
+    ? document.createTextNode('')
+    : document.createElement(fiber.type)
 
-  element.props.children.forEach(child => {
+    fiber.props.children.forEach(child => {
     render(child, dom)
   })
 
   const isProperty = key => key !== 'children'
-  Object.keys(element.props)
+  Object.keys(fiber.props)
     .filter(isProperty)
     .forEach(
-      name => dom[name] = element.props[name]
+      name => dom[name] = fiber.props[name]
     )
+  
+  return dom
+}
 
-  container.appendChild(dom)
+function render(element, container) {
+  nextUnitOfWork = {
+    dom: container,
+    props: {
+      children: [element]
+    }
+  }
+}
+
+let nextUnitOfWork = null
+
+function workLoop(deadline) {
+  let shouldYield = false
+
+  while(nextUnitOfWork && !shouldYield) {
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork)
+    shouldYield = deadline.timeRemaining() < 1
+  }
+
+  requestIdleCallback(workLoop)
+}
+
+requestIdleCallback(workLoop)
+
+function performUnitOfWork(fiber) {
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber)
+  }
+
+  if(fiber.parent) {
+    fiber.parent.dom.appendChild(fiber.dom)
+  }
+
+  const elements = fiber.props.children
+  let prevSibling
+  elements.forEach((element, index) => {
+    const newFiber = {
+      dom: null,
+      type: element.type,
+      props: element.props,
+      parent: fiber,
+    }
+
+    if (index === 0) {
+      fiber.child = newFiber
+    } else {
+      prevSibling.sibling = newFiber
+    }
+    prevSibling = newFiber
+  })
+
+  if (fiber.child) {
+    return fiber.child
+  }
+  let nextFiber = fiber
+  while (nextFiber) {
+    if (nextFiber.sibling) {
+      return nextFiber.sibling
+    }
+    nextFiber = nextFiber.parent
+  }
 }
 
 const React = {
